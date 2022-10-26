96b8cecfa639b749287766873e60a622784c525e
jswob <jacswob@gmail.com>
2022-10-26 14:45:36 +0200

    feat: add sign out button

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,16 +1,32 @@
+import axios from "axios";
 import { useContext } from "react";
+import { toast } from "react-toastify";
 import { UserContext } from "../contexts/user-context";
 
 const LoginPage = () => {
 	const {user} = useContext(UserContext);
 
-  return (
+	const signOut = () => {
+		axios.get(`${import.meta.env.VITE_API_DOMAIN}/auth/logout`, {
+			withCredentials: true,
+		})
+		.then((_data) => window.location.reload())
+		.catch(({message, name}) => {
+			toast.error(`${message} -> ${name}`)
+		});
+	}
+
+  return <>
 		<div>
 			<h1>Szybki Kalendarz!</h1>
 
 			<span>{user?.email}</span> - <span>{user?.accountType}</span>
+
+			<button onClick={signOut}>
+				Wyloguj się
+			</button>
 		</div>
-  );
+  </>;
 };
 
 export default LoginPage;
